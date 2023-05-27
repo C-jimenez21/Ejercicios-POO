8a3c327571240f3d4dfba42ae956d6f4a60f88ee
C-jimenez21 <cristianjjc21@gmail.com>
2023-05-27 17:15:03 -0500

    refactor: :recycle: Creacion de clases y funciones pertinentes para el ejemplo 4
    Se uso la estructura de codigo del anterior ejemplo y se reestructuraron las clases tanto los metodos como los atributos de las mismas, ademas de añadir nuevos eventos

--- Main.js
+++ Main.js
@@ -1,110 +1,104 @@
 //Selectores
-const color = document.getElementById("Color")
-const area = document.getElementById("Area")
+const tipoVehiculo = document.getElementById("tipoPersona")
 
-const tipoFigura = document.getElementById("tipoPersona")
-
-const radio = document.getElementById("Radio")
-const base = document.getElementById("Base")
-const altura = document.getElementById("Altura")
-
-const divRadio = document.querySelector("#divRadio")
-const divBase = document.querySelector("#divBase")
-const divAltura = document.querySelector("#divAltura")
+const marca = document.getElementById("Marca")
+const modelo = document.getElementById("Modelo")
+const velocidad = document.getElementById("Velocidad")
+const combustible = document.getElementById("Combustible")
 
 const rta = document.querySelector(".rta")
 
+const divCombustible = document.querySelector("#divCombustible")
+
 const btnCalcular = document.getElementById("accion-1")
+const btnConvertir = document.getElementById("accion-2")
+const btnDatos = document.getElementById("accion-3")
 
 //Creacion de las clases
-class Figura {
-    color
-    area
-    constructor({color = "blue", area = "0"}){
-        this.color = color
-        this.area = area
+class Vehiculo {
+    marca
+    modelo
+    velocidad
+    constructor({marca = "Avion", modelo = "2003", velocidad = 300}){
+        this.marca = marca
+        this.modelo = modelo
+        this.velocidad = velocidad
     }
-    calcularArea(){
-        return `El area es: ${this.area} m^2` 
+    acelerar(){
+        this.velocidad += 10
+        return `La velocidad actual del vehiculo de ${this.marca} y cuyo modelo es ${this.modelo} es: ${this.velocidad} KmH` 
+    }
+    static convertirKmHEnMph(km){
+        return `${km}[KmH] son equivalentes a ${km/1.60934} [MpH]`
     }
 }
 
-class Circulo extends Figura{
-    radio
-    constructor({radio = "0"}){
+class Coche extends Vehiculo{
+    combustible
+    constructor({combustible = "0"}){
         super({})
-        this.radio = radio
+        this.combustible = combustible
     }
-    calcularArea(){
-        let area = Math.PI*this.radio*this.radio
-        return `El area del ${Circulo.name} es de ${area} m^2`
-    }
-}
-
-class Rectangulo extends Figura{
-    base
-    altura
-    constructor({base = "0", altura = "0"}){
-        super({})
-        this.base = base
-        this.altura = altura
-    }
-    calcularArea(){
-        let area = this.base*this.altura
-        return `El area del ${Rectangulo.name} es de ${area} m^2`
+    acelerar(){
+        this.velocidad += 20
+        return `La velocidad actual del vehiculo de ${this.marca} y cuyo modelo es ${this.modelo} es: ${this.velocidad} KmH debido a su combustible llamado ${this.combustible}` 
     }
 }
-let figura1 = new Figura({})
-let circulo1 = new Circulo({})
-let rectangulo1 = new Rectangulo({})
+
+let vehiculo1 = new Vehiculo({})
+let coche1 = new Coche({})
 
 
-tipoFigura.addEventListener("change", ()=>{
-    if(tipoFigura.value === "1"){
-        divRadio.style.display = "contents"
-        divAltura.style.display = "none"
-        divBase.style.display = "none"
+tipoVehiculo.addEventListener("change", ()=>{
+    if(tipoVehiculo.value === "1"){
+        console.log("entro coche");
+        divCombustible.style.display = "contents"
+        btnCalcular.dataset.tipo = "coche"
+        btnConvertir.dataset.tipo = "coche"
+        btnDatos.dataset.tipo = "coche"
         
-        btnCalcular.dataset.tipo = "circulo"
-
-    }else if(tipoFigura.value === "2"){
-        divRadio.style.display = "none"
-        divAltura.style.display = "contents"
-        divBase.style.display = "contents"
-
-        btnCalcular.dataset.tipo = "rectangulo"
-
+        
     }else{
-        btnCalcular.dataset.tipo = "figura"
-        divRadio.style.display = "none"
-        divAltura.style.display = "none"
-        divBase.style.display = "none"
+        console.log("entro vehiculo");
+        btnCalcular.dataset.tipo = "vehiculo"
+        btnConvertir.dataset.tipo = "vehiculo"
+        btnDatos.dataset.tipo = "vehiculo"
+        divCombustible.style.display = "none"   
+        
+        
     }
 })
 
 btnCalcular.addEventListener("click", (e)=>{
-    if(e.target.dataset.tipo === "figura"){
-        figura1.area = area.value
-        figura1.color = color.value
-        rta.innerHTML= figura1.calcularArea()
+    if(e.target.dataset.tipo === "vehiculo"){  
+        rta.innerHTML= vehiculo1.acelerar()
+    }else if(e.target.dataset.tipo === "coche"){
+        rta.innerHTML= coche1.acelerar()
+    }
+})
 
-        rta.style.color = figura1.color
-        //console.log(figura1.hacerSonido())
-    }else if(e.target.dataset.tipo === "circulo"){
-        circulo1.radio = radio.value
-        rta.innerHTML= circulo1.calcularArea()
-        circulo1.color = color.value
-        rta.style.color = circulo1.color
-
-        //console.log(circulo1.hacerSonido())
-    }else if(e.target.dataset.tipo === "rectangulo"){
-        rectangulo1.base = base.value
-        rectangulo1.altura = altura.value
-        rta.innerHTML= rectangulo1.calcularArea()
-        rectangulo1.color = color.value
-        rta.style.color = rectangulo1.color
-
+btnConvertir.addEventListener("click", (e)=>{
+    if(e.target.dataset.tipo === "vehiculo"){
+        rta.innerHTML= Vehiculo.convertirKmHEnMph(vehiculo1.velocidad)
+        //console.log(vehiculo1.hacerSonido())
+    }else if(e.target.dataset.tipo === "coche"){
+        rta.innerHTML= Coche.convertirKmHEnMph(coche1.velocidad)
+        //console.log(coche1.hacerSonido())
     }
 })
 
 
+btnDatos.addEventListener("click", (e)=>{
+    if(e.target.dataset.tipo === "vehiculo"){
+        vehiculo1.modelo = modelo.value
+        vehiculo1.marca = marca.value
+        vehiculo1.velocidad = Number(velocidad.value )
+
+    }else if(e.target.dataset.tipo === "coche"){
+        coche1.modelo = modelo.value
+        coche1.marca = marca.value
+        coche1.velocidad = Number(velocidad.value)
+        coche1.combustible = combustible.value            
+
+    }
+})
